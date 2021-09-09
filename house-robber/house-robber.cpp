b259676c35bb57ba539804bd89ca3fa957776078
Swarnali Mondal <itsswarnali@gmail.com>
2021-09-10 02:06:05 +0530

    Time: 5 ms (5.16%), Space: 7.8 MB (21.36%) - LeetHub

---
+++ house-robber/house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,0);
+        dp[0] = 0, dp[1] = nums[0];
+        for(int i = 1;i < n; i++){
+            dp[i+1] = max(dp[i-1]+nums[i], dp[i]);
+        }
+        return dp[n];
+    }
+};
