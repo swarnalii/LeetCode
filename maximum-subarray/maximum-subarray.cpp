dc09a0f3b72397428aa8b90e9132ebcd771f2098
Swarnali Mondal <itsswarnali@gmail.com>
2021-09-12 12:22:59 +0530

    Time: 14 ms (13.49%), Space: 13.4 MB (5.28%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -4,14 +4,11 @@ public:
         int n = nums.size();
         vector<int> sum(n,0);
         sum[0] = nums[0];
+        int ans = sum[0];
         for(int i = 1; i < n; i++){
             sum[i] = max(sum[i-1] + nums[i], nums[i]);
+            ans = max(sum[i], ans);
             // cout<<"max sum at pos"<<i<<": "<< sum[i] <<endl;
-        }
-        int ans = sum[0];
-        for(int i = 0; i < n; i++){
-            if(sum[i] > ans)
-                ans = sum[i];
         }
         return ans;
     }
