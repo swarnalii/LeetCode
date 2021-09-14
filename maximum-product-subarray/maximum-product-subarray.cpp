6027685d818af14ee512cfde1443e46661be568c
Swarnali Mondal <itsswarnali@gmail.com>
2021-09-14 13:00:31 +0530

    Time: 8 ms (38.62%), Space: 11.8 MB (54.50%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.cpp
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int cur_max=ans, cur_min=ans;
         for(int i=1;i<n;i++){
             if(nums[i]<0)
-                std::swap(cur_max, cur_min);
+                swap(cur_max, cur_min);
             cur_max=max(nums[i], cur_max*nums[i]);
             cur_min=min(nums[i], cur_min*nums[i]);
             ans=max(ans, cur_max);
