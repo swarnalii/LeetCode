21657b4fb3d81a873996f4c8633cb6a9508c3ffb
Swarnali Mondal <itsswarnali@gmail.com>
2021-10-01 15:47:35 +0530

    Time: 14 ms (67.45%), Space: 10.6 MB (72.96%) - LeetHub

---
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1 = text1.size(), len2 = text2.size();
+        int dp[len1+1][len2+1]; 
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i <= len1; i++){
+            for(int j = 1; j <= len2; j++){
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[len1][len2];
+    }
+};
