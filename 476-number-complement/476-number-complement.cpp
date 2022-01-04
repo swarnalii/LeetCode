0c14d188850cecfde3e9d27f8db6dfd6ac262748
Swarnali Mondal <itsswarnali@gmail.com>
2022-01-04 13:27:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.00%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        // x = no of bits
+        // generate a number having x set of bits
+        // temp = 1<<x - 1 (doing left shift of 1 by x)
+        // n ^ temp 
+        if(num == 0)
+            return 1;
+        int x = (log2(num))+1; // finding the no of bits
+        
+        int temp= (unsigned)(1 << x) - 1;
+        
+        return temp ^ num;
+    }
+};
+
+// We are capturing log2(x) into int, so it rounds down things. For example:
+// log2(1) = 0,  but 1 bit is used
+// log2(2) = 1, but 2 bits used
+// log2(3) = 1.xx, rounded down to 1. It should be 2
+// log2(4) = 2, it should be 3.
+// So we add 1 to the result to get correct number of bits.
+
+// In next line we are doing -1 to get the mask with all 1s.
+// e.g, let's say we need a mask of 3 bits i.e., 111.
+// So, we left shift 1 by 3 = (1 << 3) = 8 = 1000
+// When we do -1, we get 8-1 = 7 = 111
