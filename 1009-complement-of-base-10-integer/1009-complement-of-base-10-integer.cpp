0ad935b11188b683e4b3d5e208037904c993699f
Swarnali Mondal <itsswarnali@gmail.com>
2022-01-04 13:24:38 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (5.99%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,10 +1,3 @@
-// class Solution {
-// public:
-//     int bitwiseComplement(int n) {
-        
-//     }
-// };
-
 class Solution {
 public:
     int bitwiseComplement( int n) {
@@ -14,12 +7,22 @@ public:
         // n ^ temp 
         if(n == 0)
             return 1;
-        int x = floor(log2(n))+1; // finding the no of bits
+        int x = (log2(n))+1; // finding the no of bits
         
         int temp= ((1 << x) - 1) ^ n;
         
         return temp;
-        
-        
     }
 };
+
+// We are capturing log2(x) into int, so it rounds down things. For example:
+// log2(1) = 0,  but 1 bit is used
+// log2(2) = 1, but 2 bits used
+// log2(3) = 1.xx, rounded down to 1. It should be 2
+// log2(4) = 2, it should be 3.
+// So we add 1 to the result to get correct number of bits.
+
+// In next line we are doing -1 to get the mask with all 1s.
+// e.g, let's say we need a mask of 3 bits i.e., 111.
+// So, we left shift 1 by 3 = (1 << 3) = 8 = 1000
+// When we do -1, we get 8-1 = 7 = 111
