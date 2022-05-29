b8ec3007450759e78e06a310bcf07b501bb9de87
Swarnali Mondal <itsswarnali@gmail.com>
2022-05-30 01:32:42 +0530

    Time: 146 ms (34.84%), Space: 15.8 MB (58.49%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // Returns true if there's a common letter between bitset of two words
+    bool checkCommon(bitset<26>& a, bitset<26>& b) {
+	    for(int i = 0; i < 26; i++) {
+            if (a[i] && b[i]) 
+                return true;
+	        // else return false;
+        }
+        return false;
+    }
+    
+    int maxProduct(vector<string>& words) {
+	    int n = words.size(), ans = 0;
+	    vector<bitset<26> > chars(n);
+	    for(int i = 0; i < n; i++) {
+		    for(auto& ch : words[i])  // map the letters to 1 if it occurs in the word
+			    chars[i][ch - 'a'] = 1;
+		        // now check with all other words and if there's no common letter, then update ans	
+		    for(int j = 0; j < i; j++){
+			    if(!checkCommon(chars[i], chars[j])) 
+				    ans = max(ans, int(size(words[i]) * size(words[j])));
+            }
+	    }   
+	    return ans;
+    }   
+};
