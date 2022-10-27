57d959f04357e59f91b623d180e0aeceb97a8eb8
Swarnali Mondal <itsswarnali@gmail.com>
2022-10-27 10:39:27 +0530

    Time: 656 ms (6.84%), Space: 113.9 MB (46.54%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,12 +4,12 @@ public:
         int sum = 0;
         int n = nums.size();
         unordered_map<int, int> mp;
-        mp[0] = 0;
+        mp[0] = -1;
         for(int i = 0; i < n; i++){
             sum += nums[i];
             if(mp.find(sum%k) == mp.end())
-                mp[sum%k] = i+1;
-            else if(mp[sum%k] < i)
+                mp[sum%k] = i;
+            else if(i - mp[sum%k] > 1)
                 return true;
         }
         return false;
