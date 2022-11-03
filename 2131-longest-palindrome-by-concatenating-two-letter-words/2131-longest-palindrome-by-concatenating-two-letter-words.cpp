14c5e8a73f291d2b51005e46cd4791acb4597b5a
Swarnali Mondal <itsswarnali@gmail.com>
2022-11-04 01:19:15 +0530

    Time: 687 ms (65.44%), Space: 168 MB (33.45%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> m;
+        int unpaired = 0, ans = 0;
+        for(auto w: words){
+            if(w[0] == w[1]){
+                if(m[w] > 0){
+                    m[w]--;
+                    unpaired--;
+                    ans += 4;
+                }
+                else{ 
+                    m[w]++;
+                    unpaired++;
+                }
+            }
+            else{
+                string rev = w;
+                reverse(rev.begin(), rev.end());
+                if(m[rev] > 0){
+                    m[rev]--;
+                    ans += 4;
+                }
+                else m[w]++;
+            }                    
+        }
+        if(unpaired > 0)
+                ans += 2;
+        return ans;
+    }
+};
+
